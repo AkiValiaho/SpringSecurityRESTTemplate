828e431fe708f3eb625b6ec21d90a87457fa4930
akivv <aki.valiaho@tamtron.fi>
2016-02-23 02:13:30 +0200

    Securerandom added to BCryptEncoder
    Added Securerandom to BCryptEncoder for additional
    safety. Input type set to password in index.html.

--- src/main/java/com/valiaho/Configuration/SecurityConfig/DevSecurityConfig.java
+++ src/main/java/com/valiaho/Configuration/SecurityConfig/DevSecurityConfig.java
@@ -3,12 +3,12 @@ package com.valiaho.Configuration.SecurityConfig;
 import com.valiaho.Configuration.Web.CustomuserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.security.SecureRandom;
 
 /**
  * Created by akivv on 7.2.2016.
@@ -25,8 +25,12 @@ public class DevSecurityConfig extends BaseSecurityConfig {
     @Override
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(10);
+        SecureRandom random = new SecureRandom();
+        //Use SecureRandom to generate a cryptographically
+        //strong key
+        return new BCryptPasswordEncoder(10, random);
     }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authenticationProvider());
