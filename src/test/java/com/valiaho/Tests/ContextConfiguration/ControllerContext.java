d58b03f2de491b065aa0868db37e32e99d95b022
AkiValiaho <Valiaho.Aki.V@student.uta.fi>
2016-05-08 21:36:21 +0300

    Removed unnecessary Bean
    No need for special MultipartResolver

--- src/test/java/com/valiaho/Tests/ContextConfiguration/ControllerContext.java
+++ src/test/java/com/valiaho/Tests/ContextConfiguration/ControllerContext.java
@@ -5,8 +5,6 @@ import com.valiaho.Service.ImageService;
 import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 /**
  * Created by akivv on 28.4.2016.
@@ -16,11 +14,6 @@ public class ControllerContext {
     @Bean
     ImageController imageController() {
         return new ImageController();
-    }
-
-    @Bean
-    MultipartResolver multipartResolver() {
-        return new CommonsMultipartResolver();
     }
 
     @Bean
