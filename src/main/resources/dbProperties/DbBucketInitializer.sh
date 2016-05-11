5e54e92d10482f2902aeffb18cef7ddd72bef1a1
Aki Väliaho <Valiaho.Aki.V@student.uta.fi>
2016-05-11 12:16:43 +0300

    Changes to DbBucket initialization script
    DbBucket initialization script clarified a little bit.

--- src/main/resources/dbProperties/DbBucketInitializer.sh
+++ src/main/resources/dbProperties/DbBucketInitializer.sh
@@ -8,6 +8,18 @@ echo $prefix
 #Initialize variables from .properties-file
 while read -r line;
 do declare ${line#$prefix};
-echo ${line#$prefix};
+	echo ${line}
+	# #-operator removes SHORTEST instance of $prefix
+	# from $line
+	echo "Using variable format ${line#$prefix}"
 done <db.properties
+echo ""
+echo "-----------------------------------------------------"
+echo "-----------------------------------------------------"
+echo "db.properties was used for initializing the variables"
+echo "-----------------------------------------------------"
+echo "-----------------------------------------------------"
+echo ""
+echo "Initializing new bucket with password: ${bucketPassword} and bucket name: ${bucket}"
+echo ""
 curl -X POST -u admin:"$bucketPassword"-d name="$bucket" -d ramQuotaMB=200 -d authType=none -d replicaNumber=2 http://127.0.0.1:8091/pools/default/buckets
