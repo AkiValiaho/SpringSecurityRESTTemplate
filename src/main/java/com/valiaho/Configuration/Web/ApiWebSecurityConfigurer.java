bac460481005a9399c442575ad55e30769b26b02
Aki Väliaho <Valiaho.Aki.V@student.uta.fi>
2016-05-11 12:18:37 +0300

    Added ApiWebSecurityConfigurer.java
    Api web security configurer added from master

---
+++ src/main/java/com/valiaho/Configuration/Web/ApiWebSecurityConfigurer.java
@@ -0,0 +1,48 @@
+package com.valiaho.Configuration.Web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * Created by akivv on 17.2.2016.
+ */
+@Configuration
+@EnableWebSecurity
+@ComponentScan(basePackages = "com.valiaho.Configuration")
+@Order(1)
+public class ApiWebSecurityConfigurer extends BaseWebSecurityConfigurationAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                //ChannelProcessingFilter is the first filter
+                //Add corsfilter before it
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER).and()
+                .addFilterBefore(corsFilter(), ChannelProcessingFilter.class)
+                .addFilterBefore(tokenProcessingLoginFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .antMatchers(HttpMethod.GET, "/productRepository/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/productRepository/**").permitAll()
+                .antMatchers("/quizapp").hasRole("USER")
+                .anyRequest().authenticated().and()
+                .csrf().disable();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        return new CorsFilter();
+    }
+
+    @Bean
+    public TokenProcessingLoginFilter tokenProcessingLoginFilter() {
+        return new TokenProcessingLoginFilter();
+    }
+}
